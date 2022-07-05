e509de0fc930e5a27ffd7e9a127aaf0585711c7c
Leszek Stęczniewski <jubeiam@gmail.com>
2022-07-05 13:04:30 +0200

    v1.1.1

--- dist/bundle.js
+++ dist/bundle.js
@@ -2345,7 +2345,7 @@ class OneOfParser {
     getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 }
 
