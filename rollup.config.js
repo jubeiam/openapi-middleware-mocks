2b2086bc20c4a516c147a24520ccf205dd50af65
Leszek Stęczniewski <jubeiam@gmail.com>
2022-10-09 11:20:12 +0200

    cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,8 @@
-// rollup.config.js
-// import typescript from '@rollup/plugin-typescript';
-import commonjs from "@rollup/plugin-commonjs";
-import ts from "@wessberg/rollup-plugin-ts";
-import pj from "@rollup/plugin-json";
+import commonjs from '@rollup/plugin-commonjs'
+import ts from '@wessberg/rollup-plugin-ts'
+import pj from '@rollup/plugin-json'
 
 export default {
-  input: "./src/index.ts",
-  plugins: [commonjs(), pj(), ts()]
-};
+    input: './src/index.ts',
+    plugins: [commonjs(), pj(), ts()],
+}
